77d6f4727e7467031b3c0d17833ccfd8854c0169
Sai Charan <charan0017@gmail.com>
2018-12-16 19:44:55 +0530

    🐞 updated bug-fixes for decimal functionality

--- src/store/actions/operations/backspace.js
+++ src/store/actions/operations/backspace.js
@@ -4,7 +4,13 @@ export default function(buttonProps, display, result) {
     if (result.calculationBlocked || result.calculationDone) {
         return clearAll(buttonProps, display, result);
     }
-    result.total = Math.floor(result.total / 10);
-    display.currentDisplay = `${result.total}`;
+    if (result.decimalUsed && display.currentDisplay.includes('.')) {
+        display.currentDisplay = display.currentDisplay.substr(0, display.currentDisplay.length - 1);
+        result.total = parseFloat(display.currentDisplay);
+    } else {
+        result.decimalUsed = false;
+        result.total = Math.floor(result.total / 10);
+        display.currentDisplay = `${result.total}`;
+    }
     return { display, result };
 };
--- src/store/actions/operations/update-display-values.js
+++ src/store/actions/operations/update-display-values.js
@@ -8,10 +8,11 @@ export default function(buttonProps, display, result) {
         clearAll(buttonProps, display, result);
     }
     if(isNumber(buttonProps.text)) {
-        if (result.decimalUsed) {
+        if (result.decimalUsed && display.currentDisplay.includes('.')) {
             display.currentDisplay += buttonProps.text;
             result.total = parseFloat(display.currentDisplay);
         } else {
+            result.decimalUsed = false;
             const number = Number(buttonProps.text);
             result.total = math.multiply(result.total, 10) + (result.total < 0 ? -number : number);
             display.currentDisplay = `${result.total}`;
