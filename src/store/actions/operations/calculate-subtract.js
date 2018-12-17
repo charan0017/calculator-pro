f16b19bf7e17322711b2ead5a24f6c7e23321dfa
Sai Charan <charan0017@gmail.com>
2018-12-17 10:53:52 +0530

    🐞 updated bug-fixes for subtraction functionality

--- src/store/actions/operations/calculate-subtract.js
+++ src/store/actions/operations/calculate-subtract.js
@@ -1,5 +1,7 @@
 import math from 'mathjs';
 
 export default function(buttonProps, display, result) {
-    result.prev = math.subtract(result.prev, result.total);
+    result.prev = result.prev
+        ? math.subtract(result.prev, result.total)
+        : result.total;
 };
