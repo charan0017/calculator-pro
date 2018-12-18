0b46381155aef0e7950307badc9376e804fc6c80
Sai Charan <charan0017@gmail.com>
2018-12-18 16:20:28 +0530

    updated navigation css

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -7,13 +7,13 @@ const navigationItems = (props) => (
     <Auxiliary>
         <ul className="navbar-nav ml-auto mb-2">
             <li className="nav-item mt-2">
-                <p className="h4 mb-0 text-capitalize">calculator</p>
+                <p className="mb-0 text-capitalize">calculator</p>
             </li>
             <NavigationItem link={"/"} exact>standard</NavigationItem>
             <NavigationItem link={"/scientific"}>scientific</NavigationItem>
             <hr />
             <li className="nav-item mt-2">
-                <p className="h4 mb-0 text-capitalize">converter</p>
+                <p className="mb-0 text-capitalize">converter</p>
             </li>
             <NavigationItem link={"#"} exact>currency</NavigationItem>
             <NavigationItem link={"#"}>volume</NavigationItem>
